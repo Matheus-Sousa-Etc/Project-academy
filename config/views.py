e18a1f43529cb7f8dd2bb0365e6a6a559f471c2a
Schneider <gabrielschneider03@gmail.com>
2026-06-02 17:21:51 -0300

    Adicionado o Include, para acessar o home pela url faça: lanch/home

--- config/views.py
+++ config/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import render
 def login_html(request):
     return render(request, 'login/login.html')
 
-def home_html(request):
-    return render(request, 'home/home.html')
+# def home_html(request):
+#     return render(request, 'home/home.html')
--- lanch/urls.py
+++ lanch/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name ="lanch"
+
 urlpatterns = [
-    path("", views.home_html),
+    path("home/", views.home_html, name="home"),
 ]
