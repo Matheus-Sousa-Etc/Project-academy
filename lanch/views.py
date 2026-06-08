92f7ed0a10873b61904ad203fb45fa8653477b83
Schneider <gabrielschneider03@gmail.com>
2026-06-08 12:22:42 -0300

    Só alguns comentarios

--- lanch/views.py
+++ lanch/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User # é mais facil usar a model propria do django por conta verificação :)
 from django.contrib.auth import authenticate, login #essa bosta serve pra autenticação do usuário
 # Create your views here.
-
 def login_html(request):
     if request.method == "GET":
         return render(request, 'login/login.html')
@@ -19,9 +18,6 @@ def login_html(request):
             return redirect ('lanch:home') #caso o usuario logue e seja autenticado, vai para a pagina home
         else:
             return HttpResponse("erro de login") #erro no login
-
-def home_html(request):
-    return render(request, 'home/home.html')
 
 def cadastro_html(request):
     if request.method== "GET":
@@ -47,17 +43,24 @@ def cadastro_html(request):
 
         return redirect ('lanch:home')
 
+def home_html(request):
+    return render(request, 'home/home.html')
+
+# Views do Menu
 def admin_html(request):
     return render(request, 'home/admin.html')
 
+# Views das aulas
 def musculo_html(request):
     return render(request, 'aulas/musculo.html')
 
 def crossfit_html(request):
     return render(request, 'aulas/crossfit.html')
 
+#Views da lanchonete
 def lanches_html(request):
     return render(request, 'lanchonete/lanches.html')
 
 def bebidas_html(request):
     return render(request, 'lanchonete/bebidas.html')
+
